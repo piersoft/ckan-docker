41747597d5f0841dbf433c531917f6a5cfd7c967
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-01-28 20:27:43 +0100

    Update 02_ckan-init.sh

--- ckan/docker-entrypoint.d/02_ckan-init.sh
+++ ckan/docker-entrypoint.d/02_ckan-init.sh
@@ -51,10 +51,28 @@ ckan config-tool $CKAN_INI "ckan.locale_default = it"
 ckan config-tool $CKAN_INI "ckan.locales_offered = it en"
 ckan config-tool $CKAN_INI "ckan.auth.create_user_via_web = false"
 ckan config-tool $CKAN_INI "ckanext.dcat.expose_subcatalogs = True"
+
 echo -e "\nCKAN init completed successfully"
 
 fi
 
+if grep -q "finitigruppi" $CKAN_INI; then
+echo "Not configuring DCATAPIT already done"
+else
 
+ckan config-tool $CKAN_INI "ckanext.dcatapit.theme_group_mapping.file = ${APP_DIR}/patches/theme_to_group.ini"
+ckan config-tool $CKAN_INI "ckanext.dcatapit.nonconformant_themes_mapping.file= ${APP_DIR}/patches/topics.json"
+ckan config-tool $CKAN_INI "ckanext.dcatapit.theme_group_mapping.add_new_groups= true"
+ckan config-tool $CKAN_INI "geonames.username=piersoft"
+ckan config-tool $CKAN_INI "ckanext.multilang.localized_resources = true"
+
+  for file in "${APP_DIR}/patches/groups/"*.json; do
+   echo "Creating group from file ${file}"
+   curl -i -H "X-CKAN-API-Key: $(ckan user token add ckan_admin gruppi | tail -n 1 | tr -d '\t')" -XPOST -d @$file http://0.0.0.0:5000/api/3/action/group_create
+  done
+
+ckan config-tool $CKAN_INI "ckan.build_groups = "finitigruppi" "
+
+echo -e "\nCKAN init groups completed successfully"
 
 
