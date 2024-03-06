672c27e643a703bbf5c118da03c345ab6039f499
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-03-06 17:49:47 +0100

    fix

--- ckan/docker-entrypoint.d/02_ckan-init.sh
+++ ckan/docker-entrypoint.d/02_ckan-init.sh
@@ -39,11 +39,14 @@ ckan dcatapit load --filename "${APP_DIR}/src/ckanext-dcatapit/vocabularies/lice
 ckan dcatapit load --filename "${APP_DIR}/patches/regions.rdf" --name regions
 
 ckan config-tool $CKAN_INI "ckan.build = "finito" "
+echo -e "\nCKAN init completed successfully"
+
+fi
 if [[ $CKAN__PLUGINS == *"dcatapit_pkg"* ]]; then
    # dcatapit_pkg settings have been configured in the .env file
    # Set API token if necessary
    echo "Set up ckanext.dcat.rdf.profiles in the CKAN config file"
-   ckan config-tool $CKAN_INI "ckanext.dcat.rdf.profiles=euro_dcat_ap it_dcat_ap"
+   ckan config-tool $CKAN_INI "ckanext.dcat.rdf.profiles=euro_dcat_ap_2 it_dcat_ap"
    ckan config-tool $CKAN_INI "ckanext.dcat.base_uri=$CKAN_SITE_URL"
 fi
 
@@ -52,28 +55,5 @@ ckan config-tool $CKAN_INI "ckan.locales_offered = it en"
 ckan config-tool $CKAN_INI "ckan.auth.create_user_via_web = false"
 ckan config-tool $CKAN_INI "ckanext.dcat.expose_subcatalogs = True"
 
-echo -e "\nCKAN init completed successfully"
-
-fi
-
-if grep -q "finitigruppi" $CKAN_INI; then
-echo "Not configuring DCATAPIT already done"
-else
-
-ckan config-tool $CKAN_INI "ckanext.dcatapit.theme_group_mapping.file = ${APP_DIR}/patches/theme_to_group.ini"
-ckan config-tool $CKAN_INI "ckanext.dcatapit.nonconformant_themes_mapping.file= ${APP_DIR}/patches/topics.json"
-ckan config-tool $CKAN_INI "ckanext.dcatapit.theme_group_mapping.add_new_groups= true"
-ckan config-tool $CKAN_INI "geonames.username=piersoft"
-ckan config-tool $CKAN_INI "ckanext.multilang.localized_resources = true"
-#ckan dcatapit load --filename "${APP_DIR}/src/ckanext-dcatapit/vocabularies/licences.rdf"
-
-  for file in "${APP_DIR}/patches/groups/"*.json; do
-   echo "Creating group from file ${file}"
-   curl -i -H "X-CKAN-API-Key: $(ckan user token add ckan_admin gruppi | tail -n 1 | tr -d '\t')" -XPOST -d @$file http://0.0.0.0:5000/api/3/action/group_create
-  done
-
-ckan config-tool $CKAN_INI "ckan.build_groups = "finitigruppi" "
-fi
-echo -e "\nCKAN init groups completed successfully"
 
 
