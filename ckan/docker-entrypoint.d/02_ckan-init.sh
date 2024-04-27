5e80ffe9a860fa12c7491edbeeca5624020fabb8
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-04-27 13:23:17 +0200

    Update 02_ckan-init.sh

--- ckan/docker-entrypoint.d/02_ckan-init.sh
+++ ckan/docker-entrypoint.d/02_ckan-init.sh
@@ -54,6 +54,7 @@ ckan config-tool $CKAN_INI "ckan.locale_default = it"
 ckan config-tool $CKAN_INI "ckan.locales_offered = it en"
 ckan config-tool $CKAN_INI "ckan.auth.create_user_via_web = false"
 ckan config-tool $CKAN_INI "ckanext.dcat.expose_subcatalogs = True"
+ckan config-tool $CKAN_INI "ckanext.dcat.normalize_ckan_format = true"
 
 
 
