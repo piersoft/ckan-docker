21ec721785ac680bb7f9cde8431a7fbad0d4da3e
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-05-05 19:38:02 +0200

    Update profiles.py

--- ckan/patches/dcatapit/ckanext/dcatapit/dcat/profiles.py
+++ ckan/patches/dcatapit/ckanext/dcatapit/dcat/profiles.py
@@ -795,6 +795,7 @@ class ItalianDCATAPProfile(RDFProfile):
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati.emilia-romagna.it/")
          if 'r_toscan' in dataset_dict.get('holder_identifier'):
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati.toscana.it/")
+            noaddsl=1
          if 'p_TN' in dataset_dict.get('holder_identifier'):
             landing_page_uri = dataset_uri(dataset_dict)
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"http://dati.trentino.it")
