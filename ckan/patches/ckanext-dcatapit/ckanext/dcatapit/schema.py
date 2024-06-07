1f49137ed6a925aa3f974b62899275179a16d6c1
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-06-07 12:23:47 +0200

    Update schema.py

--- ckan/patches/ckanext-dcatapit/ckanext/dcatapit/schema.py
+++ ckan/patches/ckanext-dcatapit/ckanext/dcatapit/schema.py
@@ -163,7 +163,7 @@ def get_custom_package_schema():
                     'placeholder': 'Non HVD',
                     'localized': True
                 },
-                {   'text': 'Metereologia',
+                {   'text': 'Dati Metereologici',
                     'name': 'Metereologia',
                     'validator': ['ignore_missing'],
                     'label': 'Metereologia',
