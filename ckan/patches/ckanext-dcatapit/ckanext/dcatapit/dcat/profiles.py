bef7fa75f171aef697c3acdcce6759fb5b63bd54
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-06-19 22:01:30 +0200

    Update profiles.py

--- ckan/patches/ckanext-dcatapit/ckanext/dcatapit/dcat/profiles.py
+++ ckan/patches/ckanext-dcatapit/ckanext/dcatapit/dcat/profiles.py
@@ -978,6 +978,10 @@ class ItalianDCATAPProfile(RDFProfile):
             landing_page_uri = dataset_uri(dataset_dict)
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://indicepa.gov.it")
             noaddsl=1
+         if 'consip' in dataset_dict.get('holder_identifier'):
+            landing_page_uri = dataset_uri(dataset_dict)
+            landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://dati.consip.it")
+            noaddsl=1
          if 'r_lomb' in dataset_dict.get('holder_identifier'):
             landing_page_uri = dataset_uri(dataset_dict)
             landing_page_uri=landing_page_uri.replace(PREF_LANDING,"https://www.dati.lombardia.it")
