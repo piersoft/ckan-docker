243d2c8e356ef7a71bce5929e3e25e25d37032fd
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-07-24 15:54:36 +0200

    Update 02_ckan-init.sh

--- ckan/docker-entrypoint.d/02_ckan-init.sh
+++ ckan/docker-entrypoint.d/02_ckan-init.sh
@@ -56,6 +56,7 @@ ckan config-tool $CKAN_INI "ckan.auth.create_user_via_web = false"
 ckan config-tool $CKAN_INI "ckanext.dcat.expose_subcatalogs = True"
 ckan config-tool $CKAN_INI "ckanext.dcat.normalize_ckan_format = true"
 ckan config-tool $CKAN_INI "geonames.username = demo"
+ckan config-tool $CKAN_INI "solr_timeout=500"
 
 
 
