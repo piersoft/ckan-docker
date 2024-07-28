8819b8c6c4c29478e788864e6c1cf3e700594397
Francesco Piero Paolicelli <piersoft2@gmail.com>
2024-07-29 01:54:25 +0200

    Update euro_dcat_ap.py

--- ckan/patches/ckanext-dcat/ckanext/dcat/profiles/euro_dcat_ap.py
+++ ckan/patches/ckanext-dcat/ckanext/dcat/profiles/euro_dcat_ap.py
@@ -579,9 +579,9 @@ class EuropeanDCATAPProfile(RDFProfile):
             if 'm_it' in dataset_dict.get('holder_identifier'):
               distribution = distribution.replace(PREF_LANDING,"https://www.interno.gov.it/")
               distribution=CleanedURIRef(distribution)
-            if 'piersoft' in dataset_dict.get('holder_identifier'):
-              distribution = distribution.replace(PREF_LANDING,"https://www.piersoft.it")
-              distribution=CleanedURIRef(distribution)
+ #            if 'piersoft' in dataset_dict.get('holder_identifier'):
+  #             distribution = distribution.replace(PREF_LANDING,"https://www.piersoft.it")
+   #            distribution=CleanedURIRef(distribution)
             if 'c_e506' in dataset_dict.get('holder_identifier'):
               distribution = distribution.replace(PREF_LANDING,"http://dati.comune.lecce.it")
               distribution=CleanedURIRef(distribution)
