1838a6b0dcf5b40de952e94b89db3a4d60e060a7
Francesco Piero Paolicelli <piersoft2@gmail.com>
2025-12-24 15:31:53 +0100

    Update 01_setup_xloader.sh

--- ckan/docker-entrypoint.d/01_setup_xloader.sh
+++ ckan/docker-entrypoint.d/01_setup_xloader.sh
@@ -2,4 +2,5 @@
    # Xloader settings have been configured in the .env file
    # Set API token if necessary
       echo "Set up ckan.datapusher.api_token in the CKAN config file"
-      ckan config-tool $CKAN_INI "ckanext.xloader.api_token=$(ckan -c $CKAN_INI user token add ckan_admin xloader | tail -n 1 | tr -d '>
+      ckan config-tool $CKAN_INI "ckanext.xloader.api_token=$(ckan -c $CKAN_INI user token add ckan_admin xloader | tail -n 1 | tr -d '\t')"
+
