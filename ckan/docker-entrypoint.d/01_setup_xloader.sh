645aeb3fb6b29feb6e0b0a0891b15ec64f82c84c
Francesco Piero Paolicelli <piersoft2@gmail.com>
2026-01-22 20:25:18 +0100

    Update 01_setup_xloader.sh

--- ckan/docker-entrypoint.d/01_setup_xloader.sh
+++ ckan/docker-entrypoint.d/01_setup_xloader.sh
@@ -7,6 +7,6 @@ else
       echo "Set up ckan.xloader api_token in the CKAN config file"
       ckan config-tool $CKAN_INI "ckanext.xloader.api_token=$(ckan -c $CKAN_INI user token add ckan_admin xloader | tail -n 1 | tr -d '\t')"
       ckan config-tool $CKAN_INI "ckanext.xloader.jobs_db.uri = postgresql://ckandbuser:ckandbpassword@db/ckandb?sslmode=disable"
-      ckan config-tool $CKAN_INI "ckan.datastore.write_url = postgresql://ckandbuser:ckandbpassword@db/datastore"
-      ckan config-tool $CKAN_INI "ckan.datastore.read_url = postgresql://datastore_ro:datastore@db/datastore"
+      ckan config-tool $CKAN_INI "ckan.datastore.write_url = postgresql://ckandbuser:ckandbpassword@db/datastore?sslmode=disable"
+      ckan config-tool $CKAN_INI "ckan.datastore.read_url = postgresql://datastore_ro:datastore@db/datastore?sslmode=disable"
 fi
